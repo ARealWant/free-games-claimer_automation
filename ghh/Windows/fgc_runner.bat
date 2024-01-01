046be4c976bf3aa93b20a089746cff9791596d12
Abdu Alawal Ibrahim <46626980+ARealWant@users.noreply.github.com>
2024-01-01 22:15:27 +0100

    Delete ghh directory

--- ghh/Windows/fgc_runner.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d /root/Documents/f_gc/free-games-claimer
-
-start "" /B cmd /c node gog
-ping 127.0.0.1 -n 10 > nul
-start "" /B cmd /c node epic-games
-ping 127.0.0.1 -n 10 > nul
-start "" /B cmd /c node prime-gaming
