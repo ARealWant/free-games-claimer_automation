6aefd3a2c8b521141a3c787af73ac4a1348b200e
Abdu Alawal Ibrahim <46626980+ARealWant@users.noreply.github.com>
2024-01-01 22:30:58 +0100

    Update fgc_runner.bat

--- Windows/fgc_runner.bat
+++ Windows/fgc_runner.bat
@@ -1,8 +1,18 @@
 @echo off
+REM Navigate to the directory where free-games-claimer is located
 cd /d /root/Documents/f_gc/free-games-claimer
 
+REM Start GOG claim
 start "" /B cmd /c node gog
-ping 127.0.0.1 -n 10 > nul
+REM Wait for 10 seconds
+timeout /nobreak /t 10 > nul
+
+REM Start Epic Games claim
 start "" /B cmd /c node epic-games
-ping 127.0.0.1 -n 10 > nul
+REM Wait for 10 seconds
+timeout /nobreak /t 10 > nul
+
+REM Start Prime Gaming claim
 start "" /B cmd /c node prime-gaming
+REM Wait for 10 seconds
+timeout /nobreak /t 10 > nul
